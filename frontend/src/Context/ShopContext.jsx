cb72eb631d5facda3a465528436344669e45345d
Anubhav Saxena <anubhav1047.be21@chitkarauniversity.edu.in>
2024-04-27 00:58:11 +0530

    dads

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -19,12 +19,12 @@ const ShopContextProvider = (props) => {
     const [all_product, setAll_Product] = useState([]);
 
     useEffect(()=>{
-        fetch('http://localhost:4000/allproducts')
+        fetch('https://shopper-y4ja.onrender.com/allproducts')
         .then((response) => response.json())
         .then((data) => setAll_Product(data));
 
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/getcart',{
+            fetch('https://shopper-y4ja.onrender.com/getcart',{
                 method: 'POST',
                 headers:{
                     Accept: 'application/form-data',
@@ -41,7 +41,7 @@ const ShopContextProvider = (props) => {
     const addToCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]:prev[itemId]+1}));
         if(localStorage.getItem('auth-token')){
-              fetch('http://localhost:4000/addtocart',{
+              fetch('https://shopper-y4ja.onrender.com/addtocart',{
                 method: 'POST',
                 headers:{
                     Accept: 'application/form-data',
@@ -58,7 +58,7 @@ const ShopContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]:prev[itemId]-1}))
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/removefromcart',{
+            fetch('https://shopper-y4ja.onrender.com/removefromcart',{
               method: 'POST',
               headers:{
                   Accept: 'application/form-data',
